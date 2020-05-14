a2ad9516674240471dac39412f2ed661b99b3a5c
Sameer Barha <sameer.barha12@gmail.com>
2020-05-14 13:52:15 +0530

    updated configs for heroku

--- app.py
+++ app.py
@@ -11,19 +11,11 @@ app = Flask(__name__,
             template_folder='src/views')
 
 #Setup app Configs
-app.config.from_pyfile('./config/dev_env.cfg')
+app.config.from_pyfile('./config/product_env.cfg')
 
 #Register the Bluprint Routes
 app.register_blueprint(index, url_prefix='/')
 app.register_blueprint(profile, url_prefix='/profile')
-
-'''
-print(app.config)
-print(f"Environment: {app.config['ENV']}")
-print(f"Debug: {app.config['DEBUG']}")
-print(f"Secret key: {app.config['SECRET_KEY']}")
-print(app.config['PORT'])
-'''
 
 def db_init():
     print('Creating Constraints ...')
@@ -38,4 +30,5 @@ def db_init():
 
 if __name__ == "__main__":
     db_init()
-    app.run(port=app.config['PORT'], debug=app.config['DEBUG'])
+    port = int(os.environ.get("PORT", app.config['PORT']))
+    app.run(port=port, debug=app.config['DEBUG'])
