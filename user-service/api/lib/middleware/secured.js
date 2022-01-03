3825cf74143c47fa525bb512d0720e787190a8ed
sameer <sameer.barha12@gmail.com>
2022-01-03 20:20:45 +0530

    fixed middleware

--- user-service/api/lib/middleware/secured.js
+++ user-service/api/lib/middleware/secured.js
@@ -1,9 +1,5 @@
 module.exports = function () {
     return function secured(req, res, next) {
-        if (req.user) { return next(); }
-        req.session.returnTo = req.originalUrl;
-        res.redirect('/login');
-
         //check if no auth token
         if (req.headers["authorization"] == undefined) {
             return res.status(403).json({
