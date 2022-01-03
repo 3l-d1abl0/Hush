cb3ab80c189f3717125513106c82f0152c470a70
sameer <sameer.barha12@gmail.com>
2022-01-03 22:50:59 +0530

    added get timeline and addpost routes & updated middleware

--- user-service/api/lib/middleware/secured.js
+++ user-service/api/lib/middleware/secured.js
@@ -1,5 +1,7 @@
+const redisClient = require('../../models/redis');
+
 module.exports = function () {
-    return function secured(req, res, next) {
+    return async function secured(req, res, next) {
         //check if no auth token
         if (req.headers["authorization"] == undefined) {
             return res.status(403).json({
@@ -7,6 +9,18 @@ module.exports = function () {
                 message: "Unauthorized"
             });
         }
+
+        let authHeader = req.headers["authorization"].replace("Bearer ", "");
+        //get user via auth token
+        let authDetails = await redisClient.hgetall(authHeader);
+        if (Object.keys(authDetails).length === 0) {
+            return res.status(403).json({
+                error: true,
+                message: "User not found!"
+            });
+        }
+
+        req.authDetails = authDetails;
 
         return next();
     };
--- user-service/api/routes/timeline.js
+++ user-service/api/routes/timeline.js
@@ -2,13 +2,76 @@ const express = require('express');
 const router = express.Router();
 
 const logger = require('../../config/logger');
-
 const redisClient = require('../models/redis');
+var secured = require('../lib/middleware/secured');
 const neo4jSession = require('../models/neo4j');
 
+var uuid = require('uuid');
 
-router.post('/', (req, res, next) => {
 
+router.get('/', secured(), async (req, res, next) => {
+
+    try {
+        const usertimeline = await redisClient.lrange(`${req.authDetails['username']}#timeline`, 0, 20);
+        return res.status(200).json({
+            error: false,
+            posts: usertimeline
+        });
+
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json({
+            error: true,
+            posts: 'Error in fetching timeline'
+        });
+    }
+
+});
+
+router.post('/addPost', secured(), (req, res, next) => {
+
+    const post = req.body.post;
+    const tags = req.body.tags;
+
+    const uuid4 = uuid.v4();
+    const timestamp = Date.now();
+
+    const querytocreatePost = `MATCH(a:User {username: '${req.authDetails['username']}'})
+        CREATE(p:Post {id: '${uuid4}', text: '${post}', timestamp: '${timestamp}'})<-[:PUBLISHED]-(a)
+        RETURN p`;
+
+
+    neo4jSession.run(querytocreatePost)
+        .then(function (result) {
+
+            const queryGetFollowers = `MATCH (u1:User{username: '${req.authDetails['username']}'})<-[r:FOLLOWS]-(u2:User) RETURN u2.username;`;
+            neo4jSession.run(queryGetFollowers)
+                .then(function (followers) {
+                    followers.records.forEach(async function (record) {
+                        logger.info(record);
+                        const result = await redisClient.rpush(`${record._fields[0]}#timeline`, `{ post: '${post}', timestamp: ${timestamp} }`);
+                    });
+                })
+                .catch(function (error) {
+                    logger.error(error);
+                    return res.status(500).json({
+                        error: true,
+                        message: error
+                    });
+                })
+
+            return res.status(200).json({
+                error: false,
+                message: "Created"
+            });
+        })
+        .catch(function (error) {
+            logger.error(error);
+            return res.status(500).json({
+                error: true,
+                message: error
+            });
+        });
 
 });
 
