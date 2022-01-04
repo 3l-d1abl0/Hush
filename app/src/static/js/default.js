79fdc1aeaeec4fb151a3b576dd67f042995f83e4
sameer <sameer.barha12@gmail.com>
2022-01-04 12:25:21 +0530

    added click function to handle posts !

--- app/src/static/js/default.js
+++ app/src/static/js/default.js
@@ -1,18 +1,47 @@
- 
-$(document).ready(function(){
-  
-    $('#search-button').on('click', function(e) {
-      if($('#search-input-container').hasClass('hdn')) {
-        e.preventDefault();
-        $('#search-input-container').removeClass('hdn')
-        return false;
-      }
-    });
-    
-    $('#hide-search-input-container').on('click', function(e) {
+
+$(document).ready(function () {
+
+  $('#search-button').on('click', function (e) {
+    if ($('#search-input-container').hasClass('hdn')) {
       e.preventDefault();
-      $('#search-input-container').addClass('hdn')
+      $('#search-input-container').removeClass('hdn')
       return false;
+    }
+  });
+
+  $('#hide-search-input-container').on('click', function (e) {
+    e.preventDefault();
+    $('#search-input-container').addClass('hdn')
+    return false;
+  });
+
+  $('.post-button').on('click', function (e) {
+    e.preventDefault();
+    const post = $('#user-post').val().trim();
+
+    if (post == "") {
+      $('.flash-message').text('Empty Post !');
+      return false;
+    }
+
+    $.ajax({
+
+      url: `/addPost`,
+      type: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      data: JSON.stringify({ 'post': post }),
+      success: function (result) {
+        console.log(result);
+      },
+      error: function (error) {
+        errorJson = JSON.parse(error.responseJSON);
+        console.log(errorJson.error);
+        $('.flash-message').text('Unable to Post ! Try Later !');
+
+      }
+
     });
-    
+
+  });
+
 });
