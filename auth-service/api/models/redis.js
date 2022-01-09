65f1bb135d2c54a9e7f167ba541319ed39e0b972
sameer <sameer.barha12@gmail.com>
2022-01-09 18:37:04 +0530

    auth/updated redis config and logged connection refused errors !

--- auth-service/api/models/redis.js
+++ auth-service/api/models/redis.js
@@ -1,8 +1,22 @@
 const Redis = require('ioredis');
+const logger = require('../../config/logger');
 
-const client = new Redis({
-    host: 'localhost',
-    port: 6379
-});
+    let client = new Redis({
+        host: 'localhost',
+        port: 6379,
+        maxRetriesPerRequest: 1,
+        retryStrategy: function(times) {
+            return 2000; // reconnect after 2 seconds
+        }
+    });
+
+    client.on('error', (e) => {
+        if(e.code == 'ECONNREFUSED'){
+            logger.error(e);
+        }
+      })
+      .on('end', (e) => {
+            logger.error(e);
+      });
 
 module.exports = client;
