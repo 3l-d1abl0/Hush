e88ec18e3b7fdcc4e3595452f5d6dcb1a3fc79a4
sameer <sameer.barha12@gmail.com>
2022-01-12 11:43:16 +0530

    added logg statements at server and redis model

--- user-service/api/models/redis.js
+++ user-service/api/models/redis.js
@@ -1,8 +1,21 @@
 const Redis = require('ioredis');
+const logger = require('../../config/logger');
 
-const client = new Redis({
-    host: 'localhost',
-    port: 6379
-});
+    let client = new Redis({
+        host: 'localhost',
+        port: 6379,
+        maxRetriesPerRequest: 1,
+        retryStrategy: function(times) {
+            return 2000; // reconnect after 2 seconds
+        },
+        enable_offline_queue: true
+    });
+
+    client.on('error', (e) => {
+        console.log('ERRRRRR!',e);
+        if(e.code == 'ECONNREFUSED'){
+            logger.error('ECONNREFUSED',e);
+        }
+    });
 
 module.exports = client;
--- user-service/server.js
+++ user-service/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 const app = require('./app');
+const logger = require('../../config/logger');
 
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app); //pass in a listner
 
 server.on('listening', function () {
+    logger.error('Server is Running !');
     console.log('Server is Running !');
+}).on('error', function (err) {
+    logger.error('Server Error : ', err);
+}).on('end', function(err){
+    logger.error('Server End : ', err);
 });
 server.listen(port);
