e106af1b038501a2af09f65301432cc9b82d63df
sameer <sameer.barha12@gmail.com>
2023-01-01 17:47:54 +0530

    added tests for SignIn

--- app/tests/integration_tests/test_routes.py
+++ app/tests/integration_tests/test_routes.py
@@ -46,7 +46,7 @@ def test_successful_signup():
     client = app.test_client()
     url = '/signup'
 
-    #Tru to register a New User
+    #Try to login a Test User
     response = client.post(url, data={
         "username" : "TestUser",
         "password": "TestUserPass",
@@ -73,3 +73,39 @@ def test_signin_page():
     response = client.get(url)
     assert response.status_code == 200
     assert b"Login to Hush" in response.data
+
+
+def test_successful_login():
+
+    app = Flask(__name__,
+            static_url_path='',
+            static_folder='../../src/static',
+            template_folder='../../src/views')
+
+    # Setup app Configs
+    app.config.from_pyfile('../../config/dev_env.cfg')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SALT'] = os.environ.get('SALT')
+
+    if os.environ.get('AUTH_SERVER') != 'None':
+        app.config['AUTH_SERVER'] = os.environ.get('AUTH_SERVICE')
+
+    if os.environ.get('USER_SERVER') != 'None':
+        app.config['USER_SERVER'] = os.environ.get('USER_SERVICE')
+
+    app.register_blueprint(index, url_prefix='/')
+
+    client = app.test_client()
+    url = '/login'
+
+    #Try to register a New User
+    response = client.post(url, data={
+        "username" : "TestUser",
+        "password": "TestUserPass",
+        "password-confirm": "TestUserPass",
+    })
+    
+    #After successful Login, page is redirected to /login
+    print(response.data)
+    assert response.status_code == 302
+    assert b"Redirecting..." in response.data
