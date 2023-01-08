07468f0bc8edb7ff356b8f90c646bbe8daab7085
Sameer <sameer.barha12@gmail.com>
2023-01-08 16:27:04 +0530

    added test for duplicate signup

--- auth-service/tests/auth.test.js
+++ auth-service/tests/auth.test.js
@@ -21,13 +21,28 @@ describe('POST /signup', () => {
         .post('/auth/signup')
         .send(usernamePass)
         .set('Accept', 'application/json');
-
-        console.log(response.body);
-        console.log('\n\n\n\n');
         
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('error', false);
         expect(response.body).toHaveProperty('message', "User created");
+      });
+
+
+      it('should try to create duplicate signup', async () => {
+        
+        const usernamePass = {
+            "username": "TestingUser",
+            "password": "TestingPass"
+        };
+  
+        const response = await request(app)
+        .post('/auth/signup')
+        .send(usernamePass)
+        .set('Accept', 'application/json');
+        
+        expect(response.status).toBe(500);
+        expect(response.body).toHaveProperty('error', true);
+        expect(response.body).toHaveProperty('message', "Unable to perform");
       });
 
 });
