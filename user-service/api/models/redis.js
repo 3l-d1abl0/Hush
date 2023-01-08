520569ff02f2fc96852ffc26a6f0d487b6eca918
Sameer <sameer.barha12@gmail.com>
2023-01-08 20:55:34 +0530

    updated config source for redis

--- user-service/api/models/redis.js
+++ user-service/api/models/redis.js
@@ -1,11 +1,18 @@
 const Redis = require('ioredis');
 const logger = require('../../config/logger');
 
+var dotenv = require('dotenv');
+
+dotenv.config({
+    path: __dirname + '/../../' + '.env'
+});
+
     let client = new Redis({
-        host: 'redis-service',
-        port: 6379,
-        maxRetriesPerRequest: 1,
+        host: process.env.REDISURI,
+        port: process.env.REDISPORT,
+        maxRetriesPerRequest: 2,
         retryStrategy: function(times) {
+            console.warn(`Retrying redis connection: attempt ${times}`);
             return 2000; // reconnect after 2 seconds
         },
         enable_offline_queue: true
@@ -16,6 +23,8 @@ const logger = require('../../config/logger');
         if(e.code == 'ECONNREFUSED'){
             logger.error('ECONNREFUSED',e);
         }
+    }).on('end', (e) => {
+        logger.error(e);
     });
 
 module.exports = client;
