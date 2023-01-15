9cd06ed25aabff6f8753741943de4f1577f2203d
Sameer <sameer.barha12@gmail.com>
2023-01-15 22:13:08 +0530

    added check for follow button

--- app/src/controller/profile.py
+++ app/src/controller/profile.py
@@ -16,28 +16,29 @@ def timeline(user_url_slug):
 
             if session["username"] == user_url_slug:
                 timeline_header = "Your recent posts ..."
+                follow_button = False
             else:
                 timeline_header = "{}'s Recent posts ... ".format(user_url_slug)
+                follow_button = True
 
             try:
                 response = requests.get(current_app.config['USER_SERVER']+"/profile/"+user_url_slug, headers={
                 "authorization": "Bearer "+session["token"]})
                 
                 response_data = response.json()
-                print(response_data)
                 if response.status_code == 200:
                     
                     if response_data['error'] == False:
-                        return render_template('profile/timeline.html', header=timeline_header, posts=response_data['posts'], username=user_url_slug, title="{}'s Profile".format(user_url_slug))
+                        return render_template('profile/timeline.html', header=timeline_header, posts=response_data['posts'], username=user_url_slug, title="{}'s Profile".format(user_url_slug), follow_button=follow_button)
                     else:
                         # some Issue
                         flash("Not able to fetch timeline ! Try Later !")
-                        return render_template('profile/timeline.html', header=timeline_header, posts=[], username=user_url_slug, title="{}'s Profile".format(user_url_slug))
+                        return render_template('profile/timeline.html', header=timeline_header, posts=[], username=user_url_slug, title="{}'s Profile".format(user_url_slug), follow_button=follow_button)
                         
                 else:
                     # Internal Server Error OR Unauthorized
                     flash("Not able to fetch your timeline ! Try again !")
-                    return render_template('profile/timeline.html', header=timeline_header, posts=[], username=user_url_slug, title="{}'s Profile".format(user_url_slug))
+                    return render_template('profile/timeline.html', header=timeline_header, posts=[], username=user_url_slug, title="{}'s Profile".format(user_url_slug), follow_button=follow_button)
 
             except requests.exceptions.RequestException as e:
                 # Service not avaiable // connection refused
