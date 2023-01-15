7e39ffdabb1a6c2b77bf77a29e46f30131bbb7cd
Sameer <sameer.barha12@gmail.com>
2023-01-15 16:40:28 +0530

    feat: added property if the user followes the requested user

--- user-service/api/routes/profile.js
+++ user-service/api/routes/profile.js
@@ -9,17 +9,28 @@ const neo4jSession = require('../models/neo4j');
 router.get('/:username', secured(), async (req, res, next) => {
 
     let username = req.params.username;
+    const self = req.authDetails.username;
+    let ifFollows = false;
+    try{
+
+    if(self!==username){
+        //Check if user follows the requested user
+        const checkIfFollows = `MATCH (u1:User{ username: '${self}'})-[r:FOLLOWS]->(u2:User { username: '${username}'}) RETURN r`;
+        let checkIfFollowsResult = await neo4jSession.run(checkIfFollows);
+        if(checkIfFollowsResult.records.length == 1){
+            ifFollows = true;
+        }
+    }
 
     const getUserPosts = `MATCH (user:User {username: '${username}' })-[:PUBLISHED]->(post:Post) RETURN post.text as text, post.timestamp as timestamp, user.username AS username ORDER BY post.timestamp DESC LIMIT 10`;
-    
-    try{
 
         const userPost = await neo4jSession.run(getUserPosts);
         return res.status(200).json({
             error: false,
             posts: userPost.records.map(function (record) {
                 return record._fields;
-            })
+            }),
+            follows: ifFollows
         });
 
     }catch (error){
