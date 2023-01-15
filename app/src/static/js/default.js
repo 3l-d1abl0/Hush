0fb9c39a02331de07fcdd9287531eb2c8ad02105
Sameer <sameer.barha12@gmail.com>
2023-01-15 21:33:37 +0530

    added follow button styling

--- app/src/static/js/default.js
+++ app/src/static/js/default.js
@@ -1,47 +1,45 @@
 
 $(document).ready(function () {
 
-  $('#search-button').on('click', function (e) {
-    if ($('#search-input-container').hasClass('hdn')) {
-      e.preventDefault();
-      $('#search-input-container').removeClass('hdn')
-      return false;
-    }
-  });
+      $('.post-button').on('click', function (e) {
+        e.preventDefault();
+        const post = $('#user-post').val().trim();
 
-  $('#hide-search-input-container').on('click', function (e) {
-    e.preventDefault();
-    $('#search-input-container').addClass('hdn')
-    return false;
-  });
+        if (post == "") {
+          $('.flash-message').text('Empty Post !');
+          return false;
+        }
 
-  $('.post-button').on('click', function (e) {
-    e.preventDefault();
-    const post = $('#user-post').val().trim();
+        $.ajax({
 
-    if (post == "") {
-      $('.flash-message').text('Empty Post !');
-      return false;
-    }
+          url: `/addPost`,
+          type: 'POST',
+          headers: { 'Content-Type': 'application/json; charset=utf-8' },
+          data: JSON.stringify({ 'post': post }),
+          success: function (result) {
+            console.log(result);
+          },
+          error: function (error) {
+            errorJson = JSON.parse(error.responseJSON);
+            console.log(errorJson.error);
+            $('.flash-message').text('Unable to Post ! Try Later !');
 
-    $.ajax({
+          }
 
-      url: `/addPost`,
-      type: 'POST',
-      headers: { 'Content-Type': 'application/json; charset=utf-8' },
-      data: JSON.stringify({ 'post': post }),
-      success: function (result) {
-        console.log(result);
-      },
-      error: function (error) {
-        errorJson = JSON.parse(error.responseJSON);
-        console.log(errorJson.error);
-        $('.flash-message').text('Unable to Post ! Try Later !');
+        });
 
-      }
+      });
 
-    });
 
-  });
+      $("#follow-button").click(function(){
+        if ($("#follow-button").text() == "+ Follow"){
 
+          $("#follow-button").text("\u2713 Following");
+
+        }else{
+          
+          $("#follow-button").text("+ Follow");
+          
+        }
+      }); 
 });
